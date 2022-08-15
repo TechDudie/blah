1fa52860eb7f12c1fbce4be46c4ff6a94eb49604
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-08-15 11:38:11 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-var text = `<h1>TechnoDot - Coder</h1>
-<p>Hello World!</p>`
+var text = `<html>
+<head>
+<title>TechnoDot</title>
+</head>
+<body>
+<h1>TechnoDot - Coder</h1>
+<p>Hello World!</p>
+<code>
+class TechnoDot:
+  def __init__(self):
+    self.food = "Pizza"
+    self.school_subjects = ["Algebra 1", "SEEK"]
+    self.activity = "Write Code and Play Minecraft"
+</code>
+</body>
+</html>`
 text = Object.assign([], text);
 var i = 0
 function print() {
